90c07b17b5d9481b0c30c4b9a6604e32be0be706
WhiteCapella <whitecapella.official@gmail.com>
2024-08-08 17:30:56 +0900

    module add

---
+++ src/movie_spark_module/data.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import os
+
+def re_partition(load_dt)
+    home_dir = os.path.expanduser("~")
+    read_path = f'{home_dir}/tmp/sparkdata/load_dt={load_dt}'
+    write_path = f'{home_dir}/data/movie/repartition/'
+    df = pd.read_parquet(read_path)
+    df['load_dt'] = load_dt
+    if(os.path.exists(f"{write_path}/load_dt={load_dt}"):
+            os.remove(f"{write_path}/load_dt={load_dt}")
+    df.to_parquet("~/data/movie/repartition/", partition_cols=['load_dt','multiMovieYn', 'repNationCd'])
+
+
